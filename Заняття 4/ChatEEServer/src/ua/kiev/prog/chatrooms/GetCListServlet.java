2e6e08c23275aff839917d3d298675aaad1700c7
VXolevchuk <79215373+VXolevchuk@users.noreply.github.com>
2021-02-17 19:36:10 +0200

    Add files via upload

---
+++ Заняття 4/ChatEEServer/src/ua/kiev/prog/chatrooms/GetCListServlet.java
@@ -0,0 +1,35 @@
+package ua.kiev.prog.chatrooms;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+
+public class GetCListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        ArrayList<String> arr = ChatRoomsList.getChatRooms();
+
+        resp.setContentType("application/json");
+
+        Gson gson = new GsonBuilder().create();
+        String json = gson.toJson(arr);
+        if (json != null) {
+            OutputStream os = resp.getOutputStream();
+            byte[] buf = json.getBytes(StandardCharsets.UTF_8);
+            os.write(buf);
+            os.close();
+        }
+    }
+
+
+
+}
